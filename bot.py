d85c9a4771ba24f3e1e30f1b9070e91e752c0c16
Vineel Sai <mail@vineelsai.com>
2023-05-23 17:17:07 +0530

    read token from file

--- bot.py
+++ bot.py
@@ -1,11 +1,23 @@
-import discord
 import os
+import sys
+
+import discord
 from dotenv import load_dotenv
+
 from features.message import message as msg
 
 load_dotenv()
 
-TOKEN = os.getenv('TOKEN')
+if os.getenv('TOKEN_FILE') is not None:
+    with open(os.getenv('TOKEN_FILE'), 'r', encoding='utf-8') as file:
+        TOKEN = file.read().strip()
+
+elif os.getenv('TOKEN') is not None:
+    TOKEN = os.getenv('TOKEN')
+
+else:
+    print("No token found")
+    sys.exit(1)
 
 intents = discord.Intents.default()
 intents.members = True
